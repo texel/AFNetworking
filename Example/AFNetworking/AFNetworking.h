6fbc568b0449e22ccc9d7a654a9e2a534d2ff0d9
Leigh Caplan <texel1@gmail.com>
2015-06-18 17:13:23 -0700

    Shared framework

---
+++ Example/AFNetworking/AFNetworking.h
@@ -0,0 +1,19 @@
+//
+//  AFNetworking.h
+//  AFNetworking
+//
+//  Created by Leigh Caplan on 6/18/15.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AFNetworking.
+FOUNDATION_EXPORT double AFNetworkingVersionNumber;
+
+//! Project version string for AFNetworking.
+FOUNDATION_EXPORT const unsigned char AFNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AFNetworking/PublicHeader.h>
+
+
